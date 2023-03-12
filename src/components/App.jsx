82da1c87a741df018bee2ebc91ea9b0305675b19
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-03-12 17:46:43 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const now = new Date().toLocaleTimeString("en-GB");
+
+  // console.log(now);
+
+  const [time, setTime] = useState(now);
+  function updateTime() {
+    const newTime = new Date().toLocaleTimeString("en-GB");
+    setTime(newTime);
+  }
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{time}</h1>
+      <button onClick={updateTime}>Get Time</button>
     </div>
   );
 }
